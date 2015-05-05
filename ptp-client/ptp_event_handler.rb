bcab792d6f498ab4c795eedbed1c9575fc264060
Marcus Frederiksen <201371033@iha.dk>
2015-05-05 11:39:07 +0200

    gcode rettet

--- ptp-client/ptp_event_handler.rb
+++ ptp-client/ptp_event_handler.rb
@@ -124,6 +124,16 @@ private
         ensure
           fd.close
         end
+
+        file_name = gcode_file
+
+        text = File.read(file_name)
+        new_contents = text.gsub(/M109 /i, "M140 #{$1}")
+        puts new_contents
+
+        # To write changes to the file, use:
+        File.open("#{file_name}", "w") {|file| file.puts new_contents }
+
       }
     end
 
